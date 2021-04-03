a613b26c21714c5d3c302a73a1e3d941d6224c5f
peach99cpp <1831427532@qqq.com>
2021-04-03 22:29:37 +0800

    循迹的相关函数

--- bsp/tracker.c
+++ bsp/tracker.c
@@ -3,3 +3,22 @@
 //
 
 #include "tracker.h"
+int weight[4]={-2,-1,1,2};
+bool tracker_on;
+int get_tracker_num(void)
+{
+    if(tracker_on) {
+        int result = 0;
+        result += weight[0] * (HAL_GPIO_ReadPin(tracker_1_GPIO_Port, tracker_1_Pin));
+        result += weight[1] * (HAL_GPIO_ReadPin(tracker_2_GPIO_Port, tracker_2_Pin));
+        result += weight[2] * (HAL_GPIO_ReadPin(tracker_3_GPIO_Port, tracker_3_Pin));
+        result += weight[3] * (HAL_GPIO_ReadPin(tracker_4_GPIO_Port, tracker_4_Pin));
+        return result;
+    }
+    else return 0;
+}
+void tracker_set(bool if_on)//是否使用循迹
+{
+    tracker_on=if_on;
+}
+
--- bsp/tracker.h
+++ bsp/tracker.h
@@ -4,5 +4,10 @@
 
 #ifndef HOLTEK_TRACKER_H
 #define HOLTEK_TRACKER_H
+
+#include <stdbool.h>
 #include "main.h"
+int get_tracker_num(void);
+extern bool tracker_on;
+void tracker_set(bool if_on);
 #endif //HOLTEK_TRACKER_H
